852b765747ce6ea21473c9ed872ded485ac8a1c5
UdaraDZ <116781547+UdaraDZ@users.noreply.github.com>
2022-10-27 10:18:14 +0530

    Update Hacktomberfest_2022_PHP_Tast02.php

--- Hacktomberfest_2022_PHP_Tast02.php
+++ Hacktomberfest_2022_PHP_Tast02.php
@@ -3,18 +3,18 @@ $favcolor = "red";
 switch ($favcolor) {
  case "red":
  echo "Your favorite color is red!";
-    //display text
+  //display text
  break;
  case "blue":
  echo "Your favorite color is blue!";
-     //display text
+  //display text
  break;
  case "green":
  echo "Your favorite color is green!";
-     //display text
+  //display text
  break;
  default:
  echo "Your favorite color is neither red, blue, nor green!";
-     //display text
+  //display text
 }
 ?>
